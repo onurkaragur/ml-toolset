4c153af9d7e97b3a657fe8d10ed608e8e0e9410f
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2025-12-04 22:09:38 +0300

    Created file structure and defined datasets

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Stores the constants used across the app.
+"""
+DEMO_DATASETS = {
+    "Titanic": "data/titanic.csv",
+    "Diabetes": "data/diabetes.csv",
+    "Iris": "data/iris.csv",
+    "Mushroom": "data/mushroom.csv",
+    "Wine Quality": "data/wine_quality.csv",
+    "Customer Churn": "data/customer_churn.csv",
+    "Breast Cancer": "data/breast_cancer"
+}
+
